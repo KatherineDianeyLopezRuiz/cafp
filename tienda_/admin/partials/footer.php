751a402e9b970732276b28b5fe016dd81d2c4368
KatherineDianeyLopezRuiz <107455527+KatherineDianeyLopezRuiz@users.noreply.github.com>
2022-11-29 07:21:52 -0600

    Add files via upload

---
+++ tienda_/admin/partials/footer.php
@@ -0,0 +1,10 @@
+<!-- Footer Section Starts -->
+<div class="footer">
+            <div class="wrapper">
+                <p class="text-center">Desarrollado - <a href="#"> Equipo 8</a></p>
+            </div>
+        </div>
+        <!-- Footer Section Ends -->
+
+    </body>
+</html>
